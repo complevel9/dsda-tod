06f34ae33af1481be0fc9697ceb1d6d96a75fdd3
complevel9 <hg5514010@gmail.com>
2025-06-06 12:54:21 +0700

    added exit digger

--- prboom2/src/dsda/args.c
+++ prboom2/src/dsda/args.c
@@ -393,6 +393,11 @@ static arg_config_t arg_config[dsda_arg_count] = {
     "prints out first time of death in tics",
     arg_null,
   },
+  [tod_arg_exitdig] = {
+    "-exitdig", NULL, NULL,
+    "prints out demo name and exit time if exits",
+    arg_null,
+  },
   [tod_arg_mhrz] = {
     "-mhrz", NULL, NULL,
     "halves red screen intensity",
